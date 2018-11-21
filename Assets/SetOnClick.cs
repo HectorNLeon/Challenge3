00e3483282b60254a12ea5fb1a2ae4f6da16a285
HectorNLeon <hectornleon98@gmail.com>
2018-11-20 22:03:21 -0600

    TicTacToe

---
+++ Assets/SetOnClick.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class SetOnClick : MonoBehaviour {
+
+    public Button button;
+    public Text butonText;
+    public string playerSide;
+
+    private GameController gameController;
+
+	public void setText()
+    {
+        butonText.text = gameController.GetPlayerSide();
+        button.interactable = false;
+        gameController.EndTurn();
+    }
+    public void SetGameControllerReference(GameController controller)
+    {
+        gameController = controller;
+    }
+}
